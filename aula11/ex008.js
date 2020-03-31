29065e66754552d3629d1e2e82c6800761244124
endroni <rubao.rcs@gmail.com>
2020-03-31 17:33:15 -0300

    condicoes simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.8 
+console.log(`A velocidade do carro é ${vel} Km/h`)
+if (vel > 60){
+    console.log(`O condutor ultrapassou a velocidade permitida! MULTADO`)
+}
+console.log(`Respeite a sinalização!`)
