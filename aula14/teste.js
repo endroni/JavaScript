5f9f04258593014441d6a2ef0a2561d9f3eee6fe
endroni <rubao.rcs@gmail.com>
2020-04-10 11:05:55 -0300

    estrutura de repeticao while (enquanto) e do while (faca enquanto)

---
+++ aula14/teste.js
@@ -0,0 +1,20 @@
+// Primeira forma
+console.log('1')
+console.log('2')
+console.log('3')
+console.log('4')
+console.log('5')
+
+// Segunda forma
+var contador = 1
+while (contador <= 5) {
+    console.log(`${contador}`)
+    contador++
+}
+
+// Terceira forma 
+var contador = 1
+do {
+    console.log(`${contador}`)
+    contador++
+} while (contador <= 5)
