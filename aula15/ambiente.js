515056c50732a488bf406c19260f2927825c4040
endroni <rubao.rcs@gmail.com>
2020-04-13 19:27:33 -0300

    estudos sobre vetores e alguns comandos js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,16 @@
 var num = [103,104,101,102]
 num[4] = 201
 num.push(302)
+num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} elementos!`)
+
+let pos = num.indexOf(403)
+
+if (pos == -1) {
+    console.log('Valor não encontrado!')
+} else {
+    console.log(`O valor 101 está na posição ${pos}`)
+}
+
 
