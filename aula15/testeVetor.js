74c04477d64ff6e181430979ae301be183738bf2
endroni <rubao.rcs@gmail.com>
2020-04-13 19:13:01 -0300

    for in - percorrendo um vetor

--- aula15/testeVetor.js
+++ aula15/testeVetor.js
@@ -1,6 +1,6 @@
 let num = [8, 5, 7, 9, 4, 6]
 console.log(num)
 
-for (let pos= 0 ; pos < num.length ; pos++) {
+for (let pos in num) {
     console.log(`A posição ${pos} do vetor, tem o valor ${num[pos]}`)
 }
